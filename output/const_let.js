8a258910d335f461286792305154d635e43b7773
Renzo Aguilar <renzo.aguilar.rojas@gmail.com>
2019-09-16 18:06:47 -0500

    instalacion babel

---
+++ output/const_let.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Caracteristica #1
+// no se puede reescribir una varieble con let y const
+var nombre = "Renzo";
+var nombre = "Consepcion"; //Si se reescribe
+
+var pais = "Perú"; //let pais = "china" -> no se puede reescribir
+
+var ciudad = "Lima"; //const ciudad = "Trujillo" -> no se puede -> no se puede reescribir
---
+++ src/const_let.js
@@ -0,0 +1,13 @@
+// Caracteristica #1
+// no se puede reescribir una varieble con let y const
+var nombre = "Renzo"
+var nombre = "Consepcion" //Si se reescribe
+
+let pais = "Perú"
+//let pais = "china" -> no se puede reescribir
+const ciudad = "Lima"
+//const ciudad = "Trujillo" -> no se puede -> no se puede reescribir
+
+
+// Caracteristica #2
+// Let y Const también tienen el escope donde no podemos llamar a la varible si estamos en una función
